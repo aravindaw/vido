2e52eff087db7c4420a6bf958617afc5b8aca5c8
Aravinda <aravindaweerasekara@gmail.com>
2016-12-29 02:56:12 +0530

    adding new files

--- config/web.php
+++ config/web.php
@@ -7,6 +7,9 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'vido',
@@ -38,15 +41,20 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
+//            'showScriptName' => false,
+//            'rules' => [
+//            ],
         ],
-        */
     ],
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+        ],
+    ],
+
     'params' => $params,
 ];
 
